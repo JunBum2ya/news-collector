67b6308b9b848abfc94d0d5d4d997307062c01aa
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-11 18:17:18 +0900

    [feat] news crawler
    news 크롤러 구현

--- src/main/kotlin/com/midas/newscollector/crawler/DaumNewsDataCrawler.kt
+++ src/main/kotlin/com/midas/newscollector/crawler/DaumNewsDataCrawler.kt
@@ -1,13 +1,78 @@
 package com.midas.newscollector.crawler
 
 import com.midas.newscollector.dto.NewsDto
+import com.midas.newscollector.util.DateUtil
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import java.io.IOException
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-class DaumNewsDataCrawler: NewsDataCrawler {
+class DaumNewsDataCrawler : NewsDataCrawler {
+    private val BASE_URL = "https://search.daum.net/search?w=news&DA=PGD&spacing=0&q=%s&sd=%s&ed=%s&period=d&p=%d"
     override fun searchNewsList(keyword: String): List<NewsDto> {
-        TODO("Not yet implemented")
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("YYYYMMddHHmmss") //날짜 형식 지정
+        val currentDateTime: LocalDateTime = DateUtil.now()
+        val startDateTime = currentDateTime.minusHours(1).format(dateTimeFormatter) //시작 시간
+        val endDateTime = currentDateTime.format(dateTimeFormatter) //검색 범위 마지막
+        return searchNewsList(keyword, startDateTime, endDateTime)
     }
 
     override fun searchNewsList(keywords: List<String>): List<NewsDto> {
-        TODO("Not yet implemented")
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("YYYYMMddHHmmss")
+        val currentDateTime = DateUtil.now()
+        val startDateTime = currentDateTime.minusHours(1).format(dateTimeFormatter)
+        val endDateTime = currentDateTime.format(dateTimeFormatter)
+        val newsDataList: MutableList<NewsDto> = java.util.ArrayList()
+        for (keyword in keywords) {
+            //키워드 마다 크롤링 실행
+            newsDataList.addAll(searchNewsList(keyword!!, startDateTime, endDateTime))
+        }
+        return newsDataList
     }
+
+    /**
+     * 1시간 전 뉴스 크롤링
+     * @param keyword 키워드
+     * @param startDateTime 검색 시작 지점
+     * @param endDateTime 검색 종료 지점
+     * @return List: NewsDto
+     * @throws IOException
+     */
+    @Throws(IOException::class)
+    private fun searchNewsList(keyword: String, startDateTime: String, endDateTime: String): List<NewsDto> {
+        val newsDtoList: MutableList<NewsDto> = ArrayList()
+        var nextButton: Element? = null
+        var page = 1
+        do {
+            val url = BASE_URL.format(keyword, startDateTime, endDateTime, page++)
+            val document = Jsoup.connect(url).get()
+            val elements = document.select("ul.c-list-basic > li")
+            newsDtoList.addAll(elements.stream().map(this::extractNewsListData).toList())
+            nextButton = document.selectFirst(".btn_next")
+        } while (nextButton != null && !nextButton.hasAttr("disable"))
+        return newsDtoList
+    }
+
+
+    /**
+     * 뉴스 데이터 추출 메소드
+     * @param element 뉴스 단건 엘리먼트
+     * @return NewsDto
+     * @throws NullPointerException
+     */
+    @Throws(NullPointerException::class)
+    private fun extractNewsListData(element: Element): NewsDto {
+        val cTitDoc = element.selectFirst(".c-tit-doc")
+        val cItemContent = element.selectFirst(".c-item-content")
+        val thumbnailElement = cItemContent.selectFirst(".thumb_bf img")
+        return NewsDto(
+            publisher = cTitDoc.selectFirst(".txt_info").text(),
+            thumbnailPath = thumbnailElement?.attr("src"),
+            title = cItemContent.selectFirst(".item-title .tit-g a").text(),
+            description = cItemContent.selectFirst(".item-contents .conts-desc a").text(),
+            url = cItemContent.selectFirst(".item-contents .conts-desc a").attr("href")
+        )
+    }
+
 }
--- src/test/kotlin/com/midas/newscollector/crawler/NewsDataCrawlerTest.kt
+++ src/test/kotlin/com/midas/newscollector/crawler/NewsDataCrawlerTest.kt
@@ -1,18 +1,63 @@
 package com.midas.newscollector.crawler
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.io.IOException
+import java.util.stream.Stream
 
+@DisplayName("Crawling Test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class NewsDataCrawlerTest {
-    @Test
-    fun givenCrawlerTypeAndKeyword_whenCrawlingKeyword_thenReturnsNewsDtoList() {
+    @DisplayName("CompanyCode와 키워드로 크롤링을 하면 NewsDto List를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("provideSingleCrawlingParameter")
+    fun givenCrawlerTypeAndKeyword_whenCrawlingKeyword_thenReturnsNewsDtoList(crawlerType: CrawlerType) {
         //given
-        val crawlerType = CrawlerType.NAVER
         val keyword = "선거"
         //when
         val crawler = NewsDataCrawlerFactory().newInstance(crawlerType)
         val newsDtoList = crawler.searchNewsList(keyword)
         //then
-        println(newsDtoList.size)
         newsDtoList.forEach { println(it.title) }
     }
+
+    private fun provideSingleCrawlingParameter(): Stream<Arguments> {
+        return Stream.of<Arguments>(
+            Arguments.of(CrawlerType.GOOGLE, "코로나"),
+            Arguments.of(CrawlerType.DAUM, "코로나"),
+            Arguments.of(CrawlerType.NAVER, "코로나")
+        )
+    }
+
+    @DisplayName("CompanyCode와 키워드 리스트로 크롤링을 하면 NewsDto List를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("provideManyKeywordCrawlingParameter")
+    @Throws(IOException::class)
+    fun givenCompanyCodeAndKeywordList_whenCrawlingNews_thenNewsDtoList(
+        companyType: CrawlerType,
+        keywords: List<String>
+    ) {
+        //given
+        val newsCrawlerFactory = NewsDataCrawlerFactory()
+        val newsCrawler = newsCrawlerFactory.newInstance(companyType)
+        //when
+        val newsDtoList = newsCrawler.searchNewsList(keywords)
+        //then
+        newsDtoList.forEach { println(it.title) }
+//        assertThat(newsDtoList).isNotEmpty();
+//        assertThat(newsDtoList.size()).isGreaterThan(0);
+    }
+
+    private fun provideManyKeywordCrawlingParameter(): Stream<Arguments> {
+        return Stream.of<Arguments>(
+            Arguments.of(CrawlerType.GOOGLE, listOf("코로나", "신약")),
+            Arguments.of(CrawlerType.DAUM, listOf("마을", "용인")),
+            Arguments.of(CrawlerType.NAVER, listOf("무역", "코딩"))
+        )
+    }
+
+
 }
