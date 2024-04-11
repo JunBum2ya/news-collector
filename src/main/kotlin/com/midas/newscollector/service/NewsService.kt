ec6d58bed4425a3e5c502374309964d08e146203
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-11 17:49:04 +0900

    [doc] new business logic comment
    뉴스 비즈니스 로직 주석 달기

--- src/main/kotlin/com/midas/newscollector/service/NewsService.kt
+++ src/main/kotlin/com/midas/newscollector/service/NewsService.kt
@@ -11,20 +11,33 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
+/**
+ * 뉴스 비즈니스 로직
+ */
 @Transactional
 @Service
 class NewsService(private val newsRepository: NewsRepository) {
+
+    /**
+     * 뉴스 검색 메소드
+     */
     @Transactional(readOnly = true)
     fun searchNews(param: NewsSearchParam, pageable: Pageable): Page<NewsDto> {
         return newsRepository.searchNews(param, pageable).map(NewsDto::of)
     }
 
+    /**
+     * 뉴스 생성 메소드
+     */
     fun createNews(newsDto: NewsDto): NewsDto {
         val news = newsRepository.getNewsByUrl(newsDto.url) ?: newsRepository.save(newsDto.toEntity())
         news.update(newsDto)
         return NewsDto.of(news)
     }
 
+    /**
+     * 뉴스 수정 메소드
+     */
     fun updateNews(newsId: Long, newsDto: NewsDto): NewsDto {
         val news = newsRepository.findById(newsId)
             .orElseThrow { CustomException(ResponseStatus.ACCESS_NOT_EXIST_ENTITY) }
@@ -32,6 +45,9 @@ class NewsService(private val newsRepository: NewsRepository) {
         return NewsDto.of(news)
     }
 
+    /**
+     * 뉴스 삭제 메소드
+     */
     fun deleteNews(newsId: Long) {
         return newsRepository.deleteById(newsId)
     }
