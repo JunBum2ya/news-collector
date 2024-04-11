d8f910323281b302992a4f019a9dd5a44303d7fa
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-11 16:46:31 +0900

    [test] news repository
    뉴스 리포지토리 구현

--- src/test/kotlin/com/midas/newscollector/repository/NewsRepositoryTest.kt
+++ src/test/kotlin/com/midas/newscollector/repository/NewsRepositoryTest.kt
@@ -3,12 +3,14 @@ package com.midas.newscollector.repository
 import com.midas.newscollector.config.JpaConfig
 import com.midas.newscollector.domain.Keyword
 import com.midas.newscollector.domain.News
+import com.midas.newscollector.dto.param.NewsSearchParam
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.ActiveProfiles
 
 @DisplayName("리포지토리 테스트 - Member")
@@ -16,6 +18,7 @@ import org.springframework.test.context.ActiveProfiles
 @Import(JpaConfig::class)
 @ActiveProfiles("testdb")
 class NewsRepositoryTest(@Autowired private val newsRepository: NewsRepository) {
+
     @DisplayName("news를 저장하면 news가 반환된다.")
     @Test
     fun givenNews_whenSaveNews_thenReturnsNews() {
@@ -32,4 +35,24 @@ class NewsRepositoryTest(@Autowired private val newsRepository: NewsRepository) 
         assertThat(savedNews.getCreatedAt()).isNotNull()
         assertThat(savedNews.getUpdatedAt()).isNotNull()
     }
+
+    @DisplayName("제목과 신문사 파라미터와 페이지네이션으로 뉴스를 조회하면 뉴스 페이지가 반환된다.")
+    @Test
+    fun given_when_then() {
+        //given
+        val pageable = PageRequest.of(0, 10)
+        val newsSearchParam = NewsSearchParam(title = "코로나 증가", publisher = "동아일보")
+        saveNews()
+        //when
+        val page = newsRepository.searchNews(newsSearchParam,pageable)
+        //then
+        assertThat(page).isNotEmpty
+        assertThat(page.totalPages).isEqualTo(1)
+        assertThat(page.totalElements).isEqualTo(1)
+    }
+
+    private fun saveNews() {
+        newsRepository.saveAndFlush(News(publisher = "동아일보", title = "코로나 증가", description = "코로나가 연속 증가합니다.", url = "corona.com"))
+    }
+
 }
