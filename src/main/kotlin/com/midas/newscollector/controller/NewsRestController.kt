99a8924b92e3fa4f384f4281063a2acf56c9a5f6
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-12 12:54:46 +0900

    [feat] 뉴스 삭제 엔드포인트 추가
    뉴스 삭제 엔드포인트 추가

--- src/main/kotlin/com/midas/newscollector/controller/NewsRestController.kt
+++ src/main/kotlin/com/midas/newscollector/controller/NewsRestController.kt
@@ -3,13 +3,16 @@ package com.midas.newscollector.controller
 import com.midas.newscollector.dto.request.NewsSearchRequest
 import com.midas.newscollector.dto.response.CommonResponse
 import com.midas.newscollector.dto.response.NewsResponse
+import com.midas.newscollector.dto.response.ResponseStatus
 import com.midas.newscollector.service.NewsService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -27,6 +30,15 @@ class NewsRestController(private val newsService: NewsService) {
     ): ResponseEntity<CommonResponse<Page<NewsResponse>>> {
         val page = newsService.searchNews(request.toNewsSearchParam(), pageable)
         return ResponseEntity.ok(CommonResponse.of(page.map { NewsResponse.from(it) }))
+    }
+
+    /**
+     * 삭제 엔드포인트
+     */
+    @DeleteMapping("/{newsId}")
+    fun deleteNewsPage(@PathVariable newsId: Long): ResponseEntity<CommonResponse<Any>> {
+        newsService.deleteNews(newsId)
+        return ResponseEntity.ok(CommonResponse.of(ResponseStatus.SUCCESS))
     }
 
 }
