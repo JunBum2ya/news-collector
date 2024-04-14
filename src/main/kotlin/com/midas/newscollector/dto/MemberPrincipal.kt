80f13700db38b731bac3158e1fefa30695b94dcf
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-14 10:43:17 +0900

    [feat] principal dto 추가
    principal dto

---
+++ src/main/kotlin/com/midas/newscollector/dto/MemberPrincipal.kt
@@ -0,0 +1,57 @@
+package com.midas.newscollector.dto
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.time.LocalDateTime
+
+data class MemberPrincipal(
+    val memberId: String,
+    val password: String,
+    val authorities: MutableList<GrantedAuthority>,
+    val email: String,
+    val createdAt: LocalDateTime?,
+    val updatedAt: LocalDateTime?
+) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return this.authorities
+    }
+
+    override fun getPassword(): String {
+        return this.password
+    }
+
+    override fun getUsername(): String {
+        return this.password
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+    companion object {
+        fun from(memberDto: MemberDto): MemberPrincipal {
+            return MemberPrincipal(
+                memberId = memberDto.memberId,
+                password = memberDto.password,
+                email = memberDto.email,
+                authorities = mutableListOf(),
+                createdAt = memberDto.createdAt,
+                updatedAt = memberDto.updatedAt
+            )
+        }
+    }
+
+}
