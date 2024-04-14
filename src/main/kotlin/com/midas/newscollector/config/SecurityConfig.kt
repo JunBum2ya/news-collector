000cf512677f325a511a5ceca6190488fda1e94b
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-14 11:25:18 +0900

    [feat] jwt 인증
    jwt 인증 구현(Spring Security)

--- src/main/kotlin/com/midas/newscollector/config/SecurityConfig.kt
+++ src/main/kotlin/com/midas/newscollector/config/SecurityConfig.kt
@@ -1,19 +1,42 @@
 package com.midas.newscollector.config
 
-import com.midas.newscollector.component.ApiKeyAuthenticator
+import com.midas.newscollector.component.JwtAuthenticationEntryPoint
+import com.midas.newscollector.component.TokenProvider
+import com.midas.newscollector.filter.CustomJwtFilter
+import com.midas.newscollector.handler.JwtAccessDeniedHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
+@EnableWebSecurity
 @Configuration
-class SecurityConfig(private val apiKeyAuthenticator: ApiKeyAuthenticator) {
+class SecurityConfig(
+    private val tokenProvider: TokenProvider,
+    val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+    val jwtAccessDeniedHandler: JwtAccessDeniedHandler
+) {
     @Bean
     @Throws(Exception::class)
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity
             .csrf { it.disable() }
-            .authorizeHttpRequests { request -> request.anyRequest().access(apiKeyAuthenticator::hasApiKey) }
+            .exceptionHandling {
+                it.authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDeniedHandler)
+            }
+            .headers { it.frameOptions { o -> o.sameOrigin() } }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { request ->
+                request.requestMatchers(
+                    "/member/login",
+                    "/member/join",
+                    "/member/issue-token"
+                ).permitAll().anyRequest().authenticated()
+            }
+            .addFilterBefore(CustomJwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
 }
